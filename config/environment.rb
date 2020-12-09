6a48dafb5825ef5eeb403fac2890f1a12f1e6c91
Tim Flood <tflood9@gmail.com>
2020-12-09 13:42:54 -0500

    Convert to sym

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require 'bundler'
 Bundler.require
 
 env = ENV.fetch("RACK_ENV", "development")
-ActiveRecord::Base.establish_connection(env)
+ActiveRecord::Base.establish_connection(env.to_sym)
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 require_all 'app'
